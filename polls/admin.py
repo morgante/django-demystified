44049617addc42d7d7fb8544300c2fb1e0268b57
Morgante Pell <morgante.pell@morgante.net>
2015-07-29 00:58:21 -0400

    register question with admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+
+# Set up our question admin
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
