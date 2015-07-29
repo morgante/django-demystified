4b163340efe794f5008c37f90a8ce2e37332109b
Morgante Pell <morgante.pell@morgante.net>
2015-07-29 00:36:30 -0400

    prevent using default secret in production

--- demystifier/settings.py
+++ demystifier/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'danger_roger')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+if (DEBUG is False and SECRET_KEY is 'danger_roger'):
+    raise KeyError("Please set a secret in production")
 
 ALLOWED_HOSTS = []
 
