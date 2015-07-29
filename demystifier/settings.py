b1cea425bd33ee1d0c2fde1dd893f32fe94f6872
Morgante Pell <morgante.pell@morgante.net>
2015-07-29 00:27:28 -0400

    NEVER store secret keys in source codee

--- demystifier/settings.py
+++ demystifier/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=v9m!1e8pf0uoidso&20oj&0#m(o&7ki=wnu079i%(-k-ntzy#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
