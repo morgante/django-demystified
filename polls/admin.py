185aaebaf81d3cace6eff6ba582ce4458b1f3b99
Morgante Pell <morgante.pell@morgante.net>
2015-07-29 00:59:10 -0400

    customize display of question in admin

--- polls/admin.py
+++ polls/admin.py
@@ -4,6 +4,9 @@ from .models import Question
 
 # Set up our question admin
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
