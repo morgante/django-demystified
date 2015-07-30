d31f5f32e26ee3543bef6d0812ca159bc5dca7f7
Morgante Pell <morgante.pell@morgante.net>
2015-07-30 02:17:05 -0400

    namespace poll info

--- demystifier/urls.py
+++ demystifier/urls.py
@@ -21,6 +21,6 @@ admin.site.site_title = 'Poll Administration'
 admin.site.index_title = 'Manage Polls'
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
