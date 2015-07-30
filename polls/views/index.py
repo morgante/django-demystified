c68055b2c027bbb8e657926cce8b640a3bdfcb5a
Morgante Pell <morgante.pell@morgante.net>
2015-07-30 02:09:30 -0400

    use render() shortcut for rendering polls index

--- polls/views/index.py
+++ polls/views/index.py
@@ -1,12 +1,8 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from ..models import Question
 
 def index(request):
     recent_questions = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'recent_questions': recent_questions,
-    })
-    return HttpResponse(template.render(context))
+    context = {'recent_questions': recent_questions}
+    return render(request, 'polls/index.html', context)
