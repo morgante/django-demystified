25d70e398843d74180694240135474a3de2cc849
Morgante Pell <morgante.pell@morgante.net>
2015-08-03 01:13:29 -0400

    prevent viewing future polls

--- polls/views/poll.py
+++ polls/views/poll.py
@@ -2,12 +2,19 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from ..models import Question, Choice
 
 class DetailView(generic.DetailView):
     pk_url_kwarg = 'question_id'
     model = Question
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(DetailView):
     template_name = 'polls/results.html'
