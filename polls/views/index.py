5a77e0f228277ca1df4432493c76324946f80527
Morgante Pell <morgante.pell@morgante.net>
2015-08-03 01:11:12 -0400

    exclude future polls from poll index

--- polls/views/index.py
+++ polls/views/index.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.utils import timezone
 
 from ..models import Question
 
@@ -8,5 +9,10 @@ class IndexView(generic.ListView):
     context_object_name = 'recent_questions'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions.
+        Exclude polls set to be published in the future.
+        """
+
+        questions = Question.objects.filter(pub_date__lte=timezone.now())
+        return questions.order_by('-pub_date')[:5]
